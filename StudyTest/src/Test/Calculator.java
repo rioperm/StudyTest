2ccccd65229905498817cd3eccdef48f659ed6ce
perm <rioperm@gmail.com>
2017-02-03 22:44:24 +0900

    test

---
+++ StudyTest/src/Test/Calculator.java
@@ -0,0 +1,22 @@
+package Test;
+
+public class Calculator {
+	int add(int i, int j){
+		return i+j;
+	}
+	
+	int subtract(int i, int j){
+		return i-j;
+	}
+	
+	int multiply(int i, int j){
+		return i*j;
+	}
+	
+	int divide(int i, int j){
+		return i/j;
+	}
+	
+	
+
+}
